3b372631f7d30cdf53a02d0a1f655c78c7cb99cb
JackChen <530216775@qq.com>
2019-01-29 21:51:09 +0800

    autoMap基础映射

---
+++ AutoMapperPractice/Model/User/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperPractice.Model.User
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
--- AutoMapperPractice/Program.cs
+++ AutoMapperPractice/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapperPractice.Model.User;
 
 namespace AutoMapperPractice
 {
@@ -10,6 +12,22 @@ namespace AutoMapperPractice
     {
         static void Main(string[] args)
         {
+            BaseMap();
+        }
+
+        public static void BaseMap()
+        {
+            var user = new User
+            {
+                Id = 1,
+                Age = 10,
+                Name = "chenjie"
+            };
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<User, UserDto>());
+            var mapper = config.CreateMapper();
+            var userDto = mapper.Map<User, UserDto>(user);
+            Console.WriteLine($"姓名{userDto.Name}年龄{userDto.Age}编号{userDto.Id}");
+            Console.ReadKey();
         }
     }
 }
