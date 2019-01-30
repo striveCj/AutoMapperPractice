ad063f2849ba345069804cff07308445a498804a
JackChen <530216775@qq.com>
2019-01-30 21:22:41 +0800

    null映射

--- AutoMapperPractice/Program.cs
+++ AutoMapperPractice/Program.cs
@@ -12,7 +12,8 @@ namespace AutoMapperPractice
     {
         static void Main(string[] args)
         {
-            BaseMap();
+            //BaseMap();
+            NullMap();
         }
 
         public static void BaseMap()
@@ -27,6 +28,14 @@ namespace AutoMapperPractice
             var mapper = config.CreateMapper();
             var userDto = mapper.Map<User, UserDto>(user);
             Console.WriteLine($"姓名{userDto.Name}年龄{userDto.Age}编号{userDto.Id}");
+            Console.ReadKey();
+        }
+        //AutoMapper将映射源映射到目标时，AutoMapper将忽略空引用异常。 这是AutoMapper默认设计。
+        public static void NullMap()
+        {
+            User user = null;
+            Mapper.Initialize(cfg=>cfg.CreateMap<User,UserDto>());
+            var userDto = Mapper.Map<User, UserDto>(user);
             Console.ReadKey();
         }
     }
