dbf43e3349690592767cfc74b7860bab42c75c49
CHENJIE-PC\Administrator <530216775@qq.com>
2019-01-31 13:31:48 +0800

    初中级使用方式

---
+++ AutoMapperPractice/Model/User/UserLowercase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperPractice.Model.User
+{
+    public class UserLowercase
+    {
+        public int id { get; set; }
+
+        public int age { get; set; }
+
+        public string name { get; set; }
+    }
+}
