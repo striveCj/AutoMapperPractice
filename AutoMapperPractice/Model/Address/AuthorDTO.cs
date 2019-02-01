dec19a047dd6af30be3caae987f421457a51485a
JackChen <530216775@qq.com>
2019-02-01 20:59:21 +0800

    model创建

---
+++ AutoMapperPractice/Model/Address/AuthorDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperPractice.Model.Address
+{
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+    }
+}
