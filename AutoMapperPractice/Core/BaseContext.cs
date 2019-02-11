6d405277b633f8a087b6cdcf8371c0ee46d0aaed
ChenJie <530216775@qq.com>
2019-02-11 22:05:26 +0800

    添加EF

---
+++ AutoMapperPractice/Core/BaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperPractice.Core
+{
+    public class BaseContext:DbContext
+    {
+        public BaseContext()
+        {
+            
+        }
+    }
+}
---
+++ AutoMapperPractice/Map/EmployeeMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapperPractice.Model.Entity;
+
+namespace AutoMapperPractice.Map
+{
+    public class EmployeeMap:EntityTypeConfiguration<EmployeeEntity>
+    {
+        public EmployeeMap()
+        {
+            ToTable("Employee");
+            HasKey(s => s.Id);
+            Property(s => s.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            Property(s => s.FirstName).HasMaxLength(20);
+            Property(s => s.LastName).HasMaxLength(20);
+        }
+    }
+}
